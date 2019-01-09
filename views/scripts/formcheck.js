508c467c6a731c530643cd22e982ea6fe5f9020f
Hamza Qadeer <hamza.qadeer@berkeley.edu>
2019-01-09 01:57:00 -0800

    some work to front-end js

--- views/scripts/formcheck.js
+++ views/scripts/formcheck.js
@@ -1,10 +1,12 @@
 $(function () {
-    $('submit').click(function () {
+    $('#form').submit(function (evt) {
         $(':input').each(function () {
             if ($(this).val() === '') {
+                evt.preventDefault();
+                window.history.back();
                 $(this).css('border-color', 'red');
                 $('contentdiv').append("<p class='text-danger pt-3 "+
-                                       "text-center'> Empty field </p");
+                                       "text-center'> Empty field! </p");
             }
         });
     });
