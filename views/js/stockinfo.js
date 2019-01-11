ed6094f698a2a97f369bc5de2a87685675d9afc7
Hamza Qadeer <hamza.qadeer@berkeley.edu>
2019-01-10 17:27:17 -0800

    added buy/sell frontend

--- views/js/stockinfo.js
+++ views/js/stockinfo.js
@@ -1,9 +1,9 @@
 $(function () {
     $('#symbol-form').submit(function (evt) {
         evt.preventDefault();
-        let symbol = [$('#symbol').val()]
+        let symbol = [$('#symbol').val()];
         let Url = `https://api.iextrading.com/1.0/stock/${symbol}`+
-                  `/quote?filter=latestPrice`
+                  `/quote?filter=latestPrice`;
         $('#price').remove();
         $.ajax({
             url: Url,
@@ -12,6 +12,16 @@ $(function () {
             success: function (data) {
                 let tag = `<p id='price'>Price: $${data.latestPrice}</p>`;
                 $('#row2').append(tag);
+                let form = '<form class="form-inline" action="buy" method="post">'+
+                           '<div class="form-group">'+
+                           '<input type="number" class="form-control">'+
+                           '<div class="button-group-sm" role="group">'+
+                           '<button type="button" class="btn btn-secondary">Buy</button>'+
+                           '<button type="button" class="btn btn-secondary">Sell</button>'+
+                           '</div>'+
+                           '</div>'+
+                           '</form>';
+                $('#form2').append(form);
             },
             error: function (req, error) {
                 let tag = `<p id='price'>Invalid symbol: "${symbol}"</p>`;
