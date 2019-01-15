95a4a7fc04af880be6c3a2a335dfb9b75017983f
Hamza Qadeer <hamza.qadeer@berkeley.edu>
2019-01-14 19:45:16 -0800

    setup file for installation

---
+++ algo_connector/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("../README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='IEX_wrapper',
+    version='0.11',
+    description='Python wrapper for IEX API and AutoStocks web app',
+    packages = setuptools.find_packages(),
+    author='Hamza Qadeer',
+    author_email='hamza.qadeer@berkeley.edu',
+    url='https://github.com/hqadeer/AutoStocks.git',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'requests'
+    ]
+)
