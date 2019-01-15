55cbb230ce38bf6a6036cf15c819cd38c0fcb463
Hamza Qadeer <hamza.qadeer@berkeley.edu>
2019-01-14 19:20:45 -0800

    finished wrapper with support for chart

--- algo_connector/wrapper.py
+++ algo_connector/wrapper.py
@@ -1,5 +1,6 @@
 import requests
 import simplejson
+import datetime
 
 class InfoError(Exception):
 
@@ -21,35 +22,73 @@ class AutoStocks:
         if any([str in login.text.lower() for str in error_strings]):
             raise ValueError('Invalid login credentials')
 
-    def get_price(self, symbol, mode='single'):
-        url_base = "https://api.iextrading.com/1.0/"
-        if mode == 'single':
-            url = url_base + "stock/{}/quote".format(symbol)
+    @staticmethod
+    def _format(lst):
+        '''Internal method to take a list of strings and return a
+           comma-separated string containing all of them'''
+        query_string = lst[0]
+        for string in lst[1:]:
+            query_string += (',' + string)
+        return query_string
+
+    def get_price(self, symbol):
+        url_base = "https://api.iextrading.com/1.0/stock/"
+        if isinstance(symbol, str):
+            url = url_base + "{}/quote".format(symbol)
             param = {'filter': 'latestPrice'}
             try:
                 price = requests.get(url, params=param).json()
             except simplejson.decoder.JSONDecodeError:
                 raise ValueError('Invalid symbol: "{}"'.format(symbol))
             return price['latestPrice']
-        elif mode == 'batch':
-            if isinstance(symbol, str):
-                raise AttributeError('Batch mode accepts an iterable of '+
-                                     'strings')
+        else:
             try:
                 iter(symbol)
             except TypeError:
-                raise AttributeError('Batch mode acccepts an iterable of '+
-                                     'strings')
-            url = url_base + "stock/market/batch"
+                raise AttributeError('Input must be a string or an '+
+                                     'iterable of strings.')
+            url = url_base + "market/batch"
             param = {'filter': 'latestPrice', 'types': 'quote',
-                     'symbols': symbol}
+                     'symbols': self._format(symbol)}
             price = requests.get(url, params=param).json()
             if len(price) < len(symbol):
                 invalids = [s for s in symbol if s not in price]
-                return ValueError('Invalid symbol(s): {}'.format(str(invalids)))
+                raise ValueError('Invalid symbol(s): {}'.format(str(invalids)))
             return {s: v['quote']['latestPrice'] for s, v in price.items()}
 
-    # To do: get_charts
+    def get_chart(self, symbols, mode='1m', filter=None):
+        url_base = "https://api.iextrading.com/1.0/stock/"
+        valid_modes = {'5y', '2y', '1y', 'ytd', '6m', '3m', '1m', '1d',
+                       'dynamic'}
+        year = datetime.datetime.now().year
+        is_date = lambda date: (isinstance(date, str) and len(date) == 8 and
+                                int(date[:4]) - year in [1, 0])
+        if mode not in valid_modes and not is_date(mode):
+            raise AttributeError('Invalid mode provided.')
+        if filter and 'date' not in filter:
+            print('Warning: "filter" argument missing "date" parameter.')
+        filter = self._format(filter)
+        if isinstance(symbols, str):
+            url = url_base + "{0}/chart/{1}".format(symbols, mode)
+            return requests.get(url, params={'filter': filter}).json()
+        else:
+            try:
+                iter(symbols)
+            except TypeError:
+                raise AttributeError('Symbol argument must be a string or '+
+                                     'an iterable of strings.')
+            url = url_base + "market/batch"
+            inputs = {
+                'symbols': self._format(symbols),
+                'types': 'chart',
+                'range': mode,
+                'filter': filter
+            }
+            chart_data = requests.get(url, params=inputs).json()
+            if len(chart_data) < len(symbols):
+                invalids = [s for s in symbols if s not in price]
+                raise ValueError('Invalid symbol(s): {}'.format(str(invalids)))
+            return {s: v['chart'] for s, v in chart_data.items()}
 
     def get_holdings(self):
         '''Returns a list of dicts representing the table shown on the web
